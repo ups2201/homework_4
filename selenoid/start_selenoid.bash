06f1ac36dcee904176eb5380d9f91b8afd716915
ups2201 <ups2201@gmail.com>
2023-01-24 22:00:50 +0300

    +

--- selenoid/start_selenoid.bash
+++ selenoid/start_selenoid.bash
@@ -1,3 +1,6 @@
+git clone git@github.com:ups2201/homework_4.git
+cd homework_4
+currentPath=$PWD
 
 docker pull selenoid/chrome:107.0
 docker pull selenoid/chrome:108.0
@@ -7,9 +10,9 @@ docker pull selenoid/opera:90.0
 docker network create selenoid_1
 docker network create selenoid_2
 
-docker run -d --name selenoid_1 -p 4445:4444 --net=selenoid_1 -v /var/run/docker.sock:/var/run/docker.sock -v /home/ups/homework/selenoid:/etc/selenoid:ro aerokube/selenoid -limit=12 -capture-driver-logs -max-timeout=0h30m0s -container-network=selenoid_1
-docker run -d --name selenoid_2 -p 4446:4444 --net=selenoid_2 -v /var/run/docker.sock:/var/run/docker.sock -v /home/ups/homework/selenoid:/etc/selenoid:ro aerokube/selenoid -limit=12 -capture-driver-logs -max-timeout=0h30m0s -container-network=selenoid_2
+docker run -d --name selenoid_1 -p 4445:4444 --net=selenoid_1 -v /var/run/docker.sock:/var/run/docker.sock -v $currentPath/selenoid:/etc/selenoid:ro aerokube/selenoid -limit=12 -capture-driver-logs -max-timeout=0h30m0s -container-network=selenoid_1
+docker run -d --name selenoid_2 -p 4446:4444 --net=selenoid_2 -v /var/run/docker.sock:/var/run/docker.sock -v $currentPath/selenoid:/etc/selenoid:ro aerokube/selenoid -limit=12 -capture-driver-logs -max-timeout=0h30m0s -container-network=selenoid_2
 
-docker run -d --name ggr -v /home/ups/homework/selenoid/ggr/grid-router:/etc/grid-router:ro --net host aerokube/ggr:latest-release
-docker run -d --name ggr-ui -p 8888:8888 -v /home/ups/homework/selenoid/ggr/grid-router/quota:/etc/grid-router/quota:ro --net host aerokube/ggr-ui:latest-release
+docker run -d --name ggr -v $currentPath/selenoid/ggr/grid-router:/etc/grid-router:ro --net host aerokube/ggr:latest-release
+docker run -d --name ggr-ui -p 8888:8888 -v $currentPath/selenoid/ggr/grid-router/quota:/etc/grid-router/quota:ro --net host aerokube/ggr-ui:latest-release
 docker run -d --name selenoid-ui --net host -p 8080:8080 aerokube/selenoid-ui --selenoid-uri=http://127.0.0.1:8888
--- wiremock/start_wiremock.bash
+++ wiremock/start_wiremock.bash
@@ -1,1 +1,5 @@
-docker run --rm -7777:8080 --name wiremock -v /home/ups/homework/wiremock/mappings:/home/wiremock/mappings wiremock/wiremock:2.35.0 --verbose
+git clone git@github.com:ups2201/homework_4.git
+cd homework_4
+currentPath=$PWD
+
+docker run --rm -7777:8080 --name wiremock -v $currentPath/wiremock/mappings:/home/wiremock/mappings wiremock/wiremock:2.35.0 --verbose
